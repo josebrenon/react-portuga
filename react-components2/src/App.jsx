c03f7afc4752918e41c05e9505a7e2fad1ef79e6
José Brenon <eibrenon88@gmail.com>
2024-06-07 17:26:09 -0300

    tee

---
+++ react-components2/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import ComponenteUm from "./components/ComponenteUm";
+export default function App() {
+  return (
+    <>
+      <ComponenteUm />
+    </>
+  );
+}
---
+++ react-components2/src/components/ComponenteUm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ComponenteUm() {
+  return (
+    <div className="componente">
+      <p>Título: Componente um</p>
+      <p>Componente mais simples de React.</p>
+    </div>
+  );
+}
