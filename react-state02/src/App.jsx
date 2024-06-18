d4fd42645a8323232ec39cf713502a13354a5be9
José Brenon <eibrenon88@gmail.com>
2024-06-18 10:50:51 -0300

    Add o react state 2

--- react-state02/src/App.jsx
+++ react-state02/src/App.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 
 export default function App() {
-  const [valor, setValor] = useState(0);
+  const [valor, setValor] = useState(100);
 
-  function incremento() {
-    return setValor(valor + 1);
+  function diminuir() {
+    setValor((valor_antigo) => valor_antigo - 1);
   }
-  function decremento() {
-    return setValor(valor - 1);
+  function aumentar() {
+    setValor((valor_antigo) => valor_antigo + 1);
   }
+
   return (
     <>
-      <h1>React hooks</h1>
-      <h3>Valor = {valor}</h3>
-      <button onClick={decremento}>Decremento</button>
-      <button onClick={incremento}>Incremento</button>
+      <h1>React Hooks - useState</h1>
+      <button onClick={diminuir}>Diminuir</button>
+      <span>{valor}</span>
+      <button onClick={aumentar}>Aumentar</button>
     </>
   );
 }
