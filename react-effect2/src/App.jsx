382d9f4bb522d4c57ca5973a11468e2026959026
José Brenon <eibrenon88@gmail.com>
2024-06-26 13:48:47 -0300

    Add o react-effect 02

--- react-effect2/src/App.jsx
+++ react-effect2/src/App.jsx
@@ -1,10 +1,43 @@
+/* eslint-disable default-case */
 import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const clientes = ["João", "Ana", "Carlos"];
+  const produtos = ["Pão", "Manteiga", "Leite"];
+  const vendas = ["1 x Pão (João)", "2 x Manteiga (Ana)", "3 x Leite (Carlos)"];
+
+  const [info, setInfo] = useState("Clientes");
+  const [valor, setValor] = useState(100);
+
+  useEffect(() => {
+    console.clear();
+    switch (info) {
+      case "clientes":
+        console.table(clientes);
+        break;
+      case "produtos":
+        console.table(produtos);
+        break;
+      case "vendas":
+        console.table(vendas);
+        break;
+    }
+  }, [info]);
+
+  useEffect(() => {
+    console.log("TESTE!");
+  }, [valor]);
+
   return (
     <>
       <h1>React Hooks - useEffect</h1>
       <hr />
+      <button onClick={() => setInfo("clientes")}>Clientes</button>
+      <button onClick={() => setInfo("produtos")}>Produtos</button>
+      <button onClick={() => setInfo("vendas")}>Vendas</button>
+      <h3>{info}</h3>
+      <h3>{valor}</h3>
+      <button onClick={() => setValor(valor + 10)}>Aumentar</button>
     </>
   );
 }
