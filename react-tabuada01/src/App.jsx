8951fe5b5725861e6b5c177508303653ef2bfb23
José Brenon <eibrenon88@gmail.com>
2024-07-05 15:57:13 -0300

    Add o exercício tabuada

---
+++ react-tabuada01/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+export default function App() {
+  const [num, setNum] = useState(1);
+
+  function multiplicar() {
+    if (isNaN(num)) return [];
+    let produtos = [];
+    for (let m = 1; m <= 10; m++) {
+      produtos.push(`${num} x ${m} = ${num * m}`);
+    }
+    return produtos;
+  }
+
+  return (
+    <>
+      <h1>React - Tabuada</h1>
+      <hr />
+      <input
+        type="number"
+        value={num}
+        onChange={(e) => {
+          setNum(parseInt(e.target.value));
+        }}
+      />
+      {multiplicar().map((item) => {
+        return <p key={uuid()}>{item}</p>;
+      })}
+    </>
+  );
+}
