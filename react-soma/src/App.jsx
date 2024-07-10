6c61490c0611e0773e41a347f087913f663d2889
José Brenon <eibrenon88@gmail.com>
2024-07-10 08:56:25 -0300

    Corrigindo o exercício

--- react-soma/src/App.jsx
+++ react-soma/src/App.jsx
@@ -5,21 +5,32 @@ export default function App() {
   const [nDois, setNDois] = useState(0);
   const [nTres, setNTres] = useState(0);
   const [nQuatro, setNQuatro] = useState(0);
+  const [soma, setSoma] = useState(0);
+  const [subtração, setSubtração] = useState(0);
 
   function somar() {
     if (isNaN(nUm) || isNaN(nDois)) return [];
 
-    const soma = [];
-    soma.push(`${nUm} + ${nDois} = ${nUm + nDois}`);
-    return soma;
+    setSoma(() => {
+      return `Resultado: ${nUm + nDois}`;
+    });
+    // soma.push(`${nUm} + ${nDois} = ${nUm + nDois}`);
+    // return soma;
   }
 
   function subtrair() {
     if (isNaN(nTres) || isNaN(nQuatro)) return [];
-    const subtração = [];
-    subtração.push(`${nTres} - ${nQuatro} = ${nTres - nQuatro}`);
-    return subtração;
+    setSubtração(() => {
+      return `Resultado: ${nTres - nQuatro}`;
+    });
   }
+
+  // function subtrair() {
+  //   if (isNaN(nTres) || isNaN(nQuatro)) return [];
+  //   const subtração = [];
+  //   subtração.push(`${nTres} - ${nQuatro} = ${nTres - nQuatro}`);
+  //   return subtração;
+  // }
 
   return (
     <>
@@ -40,9 +51,11 @@ export default function App() {
           setNDois(parseInt(e.target.value));
         }}
       />
-      {somar().map((item) => {
+      <button onClick={somar}>Somar</button>
+      {soma === 0 ? <p>Resultado:</p> : <p>{soma}</p>}
+      {/* {somar().map((item) => {
         return <p>{item}</p>;
-      })}
+      })} */}
       <hr />
       <h1>Subtraindo Números</h1>
       <hr />
@@ -61,9 +74,27 @@ export default function App() {
           setNQuatro(parseInt(e.target.value));
         }}
       />
+      <button onClick={subtrair}>Subtrair</button>
+      {subtração === 0 ? <p>Resultado:</p> : <p>{subtração}</p>}
+      {/*
+      <input
+        type="number"
+        value={nTres}
+        onChange={(e) => {
+          setNTres(parseInt(e.target.value));
+        }}
+      />
+      {" + "}
+      <input
+        type="number"
+        value={nQuatro}
+        onChange={(e) => {
+          setNQuatro(parseInt(e.target.value));
+        }}
+      />
       {subtrair().map((item) => {
         return <p>{item}</p>;
-      })}
+      })} */}
     </>
   );
 }
